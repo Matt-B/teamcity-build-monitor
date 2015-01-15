6400ef496fe36777f646482a579932caf1702efe
Matt Bailey <matthew.bailey@7digital.com>
2015-01-15 12:13:52 +0000

    Move inline CSS to a static file and add some more colour.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var express = require('express')
 var app = express()
 var teamcityUrl = process.env.TEAMCITY_HOST
 
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   getBuildInformation(function(builds) {
