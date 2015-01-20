781c932fa4bfb8d1672849233f7451f8f0e1c9eb
Matt Bailey <matthew.bailey@7digital.com>
2015-01-20 15:53:11 +0000

    Move teamcity interaction into its own module and refactor a bit.

--- app.js
+++ app.js
@@ -1,90 +1,22 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var express = require('express')
-var app = express()
-var teamcityUrl = process.env.TEAMCITY_HOST;
+var express = require('express');
+var teamcity = require('./lib/teamcity');
+var app = express();
 var filterString = process.env.TEAMCITY_PROJECT_FILTER; 
-
-console.log(filterString);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  getBuildInformation(function(builds) {
-    if(filterString) {
-      filterBuildsByProjectTitle(builds, filterString, function(filteredBuilds) {      
-        filterBuildsByState(filteredBuilds, 'failing', function(failingBuilds) {
-          filterBuildsByState(filteredBuilds, 'investigate', function(investigatingBuilds) {
-            res.render('index', {
-              failingBuilds: failingBuilds,
-              investigatingBuilds: investigatingBuilds
-            });
-          });
-        });
-      });
-    } else {
-      filterBuildsByState(builds, 'failing', function(failingBuilds) {
-        filterBuildsByState(builds, 'investigate', function(investigatingBuilds) {
-          res.render('index', {
-            failingBuilds: failingBuilds,
-            investigatingBuilds: investigatingBuilds
-          });
-        });
-      });
-    }
+  teamcity.getBuilds(function(builds) {
+    teamcity.filterAndSortBuildsByState(builds, filterString, function(sortedAndFilteredBuilds) {
+      res.render('index', sortedAndFilteredBuilds);
+    });
   });
-})
+});
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address
   var port = server.address().port
-
   console.log('App listening at http://%s:%s', host, port)
-
 })
-
-
-function getBuildInformation(callback) {
-  request(teamcityUrl + '/externalStatus.html', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      $ = cheerio.load(body);
-      var buildConfigurations = $('.tcTable .buildConfigurationName');
-      var builds = []; 
-      for (i = 0; i < buildConfigurations.length; i++) { 
-        buildConfig = $(buildConfigurations[i]);
-        var project = buildConfig.parent().parent().find('tr .projectName a').text();
-        var name = buildConfig.find('a').text();
-        var buildStatus = buildConfig.find('img').attr('title');
-        builds.push({
-          project: project,
-          name: name,
-          buildStatus: buildStatus
-        });
-      };
-      callback(builds);
-    }
-  })
-};
-
-function filterBuildsByProjectTitle(builds, projectName, callback) {
-  var filteredBuilds = [];
-  for (i = 0; i < builds.length; i++) {
-    if(builds[i].project.indexOf(projectName) != -1) {
-      filteredBuilds.push(builds[i]);
-    }
-  };
-  callback(filteredBuilds);
-};
-
-function filterBuildsByState(builds, state, callback) {
-  var filteredBuilds = [];
-  for (i = 0; i < builds.length; i++) {
-    if(builds[i].buildStatus.indexOf(state) != -1) {
-      filteredBuilds.push(builds[i]);
-    }
-  };
-  callback(filteredBuilds);
-};
